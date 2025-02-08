7a56ce62ede53583062a6e21d167116d9a5cdae6
yassine safraoui <yassine.safraoui@grenoble-inp.org>
2025-02-08 12:18:03 +0100

    fixed date parsing error

--- src/trigger/processors/alten.ts
+++ src/trigger/processors/alten.ts
@@ -1,16 +1,9 @@
 import { Job } from '../../schema';
+import { parseCustomDate } from '../helpers';
 import { JobProcessor } from './types'
 import { JSDOM } from "jsdom";
 
-declare global {
-    interface String {
-        sanitize(): string;
-    }
-}
 
-String.prototype.sanitize = function (): string {
-    return this.replaceAll("\n", "").replaceAll("\t", "").trim();
-};
 
 export const altentProcessor: JobProcessor = async (source) => {
     const request = await fetch(source.url);
@@ -19,12 +12,16 @@ export const altentProcessor: JobProcessor = async (source) => {
     }
     const document = new JSDOM(await request.text()).window.document;
     return Array.from(document.getElementsByClassName("is-style-card-default wp-block-webfactory-card"))
-        .map(offer => ({
-            url: offer.querySelector("a")?.href ?? "",
-            title: offer.querySelector("a")?.textContent?.sanitize() ?? "",
-            publishedAt: new Date(offer.querySelector(".card-date")?.textContent?.sanitize() ?? "").getTime(),
-            sourceId: source.id!,
-            location: offer.querySelector(".location-list")?.textContent
-                ?.replaceAll("\n", "").replaceAll("\t", "") ?? ""
-        } satisfies Job))
+        .map((offer): Job | null => {
+            let extracted_date = parseCustomDate(offer.querySelector(".card-date")?.textContent?.sanitize())
+            if (!extracted_date) return null
+            return ({
+                url: offer.querySelector("a")?.href ?? "",
+                title: offer.querySelector("a")?.textContent?.sanitize() ?? "",
+                publishedAt: extracted_date.getTime(),
+                sourceId: source.id!,
+                location: offer.querySelector(".location-list")?.textContent
+                    ?.replaceAll("\n", "").replaceAll("\t", "") ?? ""
+            })
+        }).filter(e => e != null)
 }
